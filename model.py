eff274623386080194b174907d490c40b48e5d24
Brady <kite.alva@gmail.com>
2023-01-03 13:38:40 -0700

    css-add

--- model.py
+++ model.py
@@ -4,6 +4,22 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+class User(db.Model):
+    
+    __tablename__ = 'users'
+    
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}> username={self.username}>'
+    
+    @classmethod
+    def create(cls, email, username, password):
+        
+        return cls(email=email, username=username, password=password)
 
 
 def connect_to_db(flask_app, db_uri=os.environ["POSTGRES_URI"], echo=True):
